4274ec25fce37f4c997afce8618e59709b619d60
Shaikh Ansar <skansar9731@gmail.com>
2026-05-08 20:11:14 +0530

    Fixed home redirect

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def home_redirect(request):
+
+    return redirect('/login/')
+
 
 urlpatterns = [
+
+    path(
+        '',
+        home_redirect
+    ),
 
     path(
         'admin/',
